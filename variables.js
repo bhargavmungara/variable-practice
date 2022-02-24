714448a1f692b7d222f6947bb0bc30e24af3407d
bhargav <bhargav@bhargavs-MacBook-Air.local>
2022-02-24 12:21:29 +0530

    Add annotations

--- variables.js
+++ variables.js
@@ -5,16 +5,26 @@ one message is
 
 farming is very important
 
+//variable dogname is declared and initialised to oscar
 var dog="oscar"
 
+//variable dogage is declared and initialised to 11 
 var dogAge=11
 
+//varible male is declared and initialised to false
 var male=false
 
+//variable dogch is declared and initialised to 34
 var dogch=34
 
+//varible dogtoys is declared and initialised to 100
 var dogToys=100
 
+//varible teeth is declared and initialised to false
 var teeth=false
 
+//varible fur is declared and initialised to true
 var fur=true
+
+//Two variable -  fur,dogToys are logged into the console 
+console.log(fur,dogToys)
